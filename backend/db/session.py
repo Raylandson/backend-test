1254acb54156c29cebe8e7dfcf7d55de53f6e21b
Raylandson Cesário <www.raylandsoncs@gmail.com>
2024-10-23 21:41:20 -0300

    Update session.py

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,13 +1,20 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-# Replace <db_password> with the actual password for your admin user
-MONGO_URI = "mongodb+srv://admin:q4n6K0W3MeYFl4xn@travels.3dckthr.mongodb.net/?retryWrites=true&w=majority"
+load_dotenv()
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+# Replace <db_password> with t'he actual password for your admin user
+MONGO_URI = f"mongodb+srv://{db_user}:{db_password}@travels.3dckthr.mongodb.net/?retryWrites=true&w=majority"
 
 # You can specify your database name here if you already know it
 MONGO_DB = "travels"
 
 # Create the MongoClient and specify the database
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI,
+    connecttimeoutms=60000,
+    serverselectiontimeoutms=60000)
 db = client[MONGO_DB]
 
 def get_db():
